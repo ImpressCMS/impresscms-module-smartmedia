2fab0f334da75d1c68ec9851074e580028c45232
Sina Asghari <stranger@impresscms.org>
2008-04-13 09:31:29 +0000

    Translating a couple of modules to Persian (smartprofile) & (smartpartner) & (smartmedia) & (members) and removing all Thumbs.db files from modules.
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/smartmedia/trunk@1556 7a4d4d67-0c3a-0410-b24c-89d50c169223

---
+++ language/persian/blocks.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+* $Id: blocks.php,v 1.2 2005/06/02 13:33:37 malanciault Exp $
+* Module: SmartMedia
+* Author: The SmartFactory <www.smartfactory.ca>
+* Translator: stranger <www.impresscms.ir>
+* Licence: GNU
+*/
+
+define("_MB_SMEDIA_TRUNCATE_TITLE", "بیشترین تعداد کارکتر در عنوان:");
+define("_MB_SMEDIA_MAX_CLIPS", "تعداد کلیپ ها برای نمایش:");
+
+?>
